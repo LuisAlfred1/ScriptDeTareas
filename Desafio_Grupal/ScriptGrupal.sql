90bbc2490b063cf29d4a458924f40403bfe498c0
Luis Reyes <alfredluis177@gmail.com>
2025-04-08 13:24:57 -0600

    Se resolvió los primeros 2 retos del ejercicios grupal

--- Desafio_Grupal/ScriptGrupal.sql
+++ Desafio_Grupal/ScriptGrupal.sql
@@ -1,2 +1,51 @@
---En equipos de 2 a 4 personas, resuelvan los siguientes desafíos utilizando consultas SQL sobre la base de datos de ventas.
+--Grupo: Walter Mijangos y Luis Reyes
 
+
+/*
+🔹 Reto 1 – Top compradores del año
+    Mostrar los 5 clientes que más dinero han gastado en total durante el último año, ordenados del mayor al menor.
+*/
+
+SELECT TOP 5
+    c.cliente_id,
+    CONCAT(c.nombre,' ',c.apellido) as Cliente,
+    SUM(V.total_venta) AS Total_De_Gastos
+FROM cli.clientes c 
+JOIN sell.ventas v on c.cliente_id = v.cliente_id
+WHERE YEAR(V.fecha_venta) = YEAR(GETDATE()) - 1  --Año 2024
+GROUP BY c.cliente_id, CONCAT(c.nombre,' ',c.apellido)
+ORDER BY Total_De_Gastos DESC;
+
+
+/*
+🔹 Reto 2 – Categorías sin productos vendidos
+    Encontrar las categorías que no han tenido ningún producto vendido.
+*/
+
+SELECT 
+    c.categoria_id,
+    c.categoria
+FROM sell.categoria as c 
+LEFT JOIN sell.productos as p ON c.categoria_id = p.categoria_id
+LEFT JOIN sell.detalle_ventas dv ON p.producto_id = dv.producto_id
+WHERE dv.producto_id IS NULL;
+
+/*
+🔹 Reto 3 – Días con mayor volumen de ventas
+   Listar los 3 días con más cantidad de productos vendidos (sumando todas las ventas del día).
+*/
+
+
+
+
+/*
+🔹 Reto 4 – Productos con mejor desempeño en stock
+    Mostrar los productos que han vendido más del 50% de su stock actual.
+*/
+
+
+
+/*
+🔹 Reto 5 – Clientes sin compras pero con carritos
+    Encontrar los clientes que no han realizado ninguna compra, pero tienen al menos un carrito creado.
+*/
