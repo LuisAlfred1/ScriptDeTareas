6a89c5d69578eaf43689bbc6fe5332a5890187c4
Luis Reyes <alfredluis177@gmail.com>
2025-04-09 12:22:11 -0600

    Se resolvió los primeros 2 retos del ejercicios grupal

--- Desafio_Grupal/ScriptGrupal.sql
+++ Desafio_Grupal/ScriptGrupal.sql
@@ -1,6 +1,5 @@
 --Grupo: Walter Mijangos y Luis Reyes
 
-
 /*
 🔹 Reto 1 – Top compradores del año
     Mostrar los 5 clientes que más dinero han gastado en total durante el último año, ordenados del mayor al menor.
@@ -12,7 +11,7 @@ SELECT TOP 5
     SUM(V.total_venta) AS Total_De_Gastos
 FROM cli.clientes c 
 JOIN sell.ventas v on c.cliente_id = v.cliente_id
-WHERE YEAR(V.fecha_venta) = YEAR(GETDATE()) - 1  --Año 2024
+WHERE YEAR(V.fecha_venta) = YEAR(GETDATE()) - 4  --Año 2021 // La BD de Ecommerce tiene registros del año 2021 
 GROUP BY c.cliente_id, CONCAT(c.nombre,' ',c.apellido)
 ORDER BY Total_De_Gastos DESC;
 
